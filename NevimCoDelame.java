1bcb5f81bc8fe1190c864621a4323554777f02e8
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-01 09:10:34 +0100

    Add files via upload

---
+++ NevimCoDelame.java
@@ -0,0 +1,28 @@
+package nevimcodelame;
+
+import java.util.Arrays;
+
+public class NevimCoDelame {
+
+    public static void main(String[] args) {
+        String[] slovo;
+        slovo = new String[14];
+        slovo[0]="Být";
+        slovo[1]="Bydlit";
+        slovo[2]="Obyvatel";
+        slovo[3]="Byt";
+        slovo[4]="Příbytek";
+        slovo[5]="Nábytek";
+        slovo[6]="Dobytek";
+        slovo[7]="Obyčej";
+        slovo[8]="Bystrý";
+        slovo[9]="Bylina";
+        slovo[10]="Kobyla";
+        slovo[11]="Babyka";
+        slovo[12]="Býk";
+        slovo[13]="Přibyslav";
+        System.out.println(Arrays.toString(slovo));
+        
+    }
+    
+}
